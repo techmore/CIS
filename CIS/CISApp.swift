b05725fa092186a09f3a6f21f4abd495b8207eb0
techmore.co <sdolbec1@gmail.com>
2025-04-01 14:12:56 -0400

    Initial Commit

---
+++ CIS/CISApp.swift
@@ -0,0 +1,17 @@
+//
+//  CISApp.swift
+//  CIS
+//
+//  Created by techmore on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
